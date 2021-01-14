54b3272de7f780de77163edc42f5e20f06615e36
Danik Roman <danirom0401@gmail.com>
2021-01-14 13:02:43 +0200

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
     <div>
       <Header />
       <div className = "container" >
-        <Route exact path = "/" render = { () => <ListContaier /> } />
+        <Route exact path = "/shop" render = { () => <ListContaier /> } />
         <Route path = "/cart" render = { () => <CartContainer /> } />
       </div>
     </div>
